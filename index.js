909686e853a4fe6c75c9b5958064b82d57b745ab
coderdal <agircoder@gmail.com>
2022-05-26 21:08:25 +0300

    CORS removed

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const express = require("express");
 const app = express();
 const { v4: uuidv4 } = require("uuid");
-const cors = require("cors");
 
 const port = 3001;
-
-const corsOption = {
-  origin: ["https://givemeidea.netlify.app"],
-};
-
-app.use(cors(corsOption));
 
 const data = [
   /* CSS */
