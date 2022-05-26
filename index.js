bc3599858823b93719441d619c8aee98d42d50b7
coderdal <agircoder@gmail.com>
2022-05-26 21:16:38 +0300

    CORS

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const { v4: uuidv4 } = require("uuid");
+var cors = require("cors");
 
 const port = 3001;
+
+app.use(cors({ origin: "*" }));
 
 const data = [
   /* CSS */
